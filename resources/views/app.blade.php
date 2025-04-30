2123d73fb566485a8bdf45add05971b65ae59ee1
Miguel Elias <miguelelias20050806@gmail.com>
2025-04-30 00:58:31 -0300

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LinksRoom</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    {{ $slot }}
+
+</body>
+</html>
