792da7813501165a6df8f3708c4348c6e1defa44
Miguel <miguelelias20050806@gmail.com>
2025-05-07 18:03:10 -0300

    :construct: feat: sorting front-end

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layout.app>
 
-    <main class="flex p-4 h-screen w-screen bg-neutral">
+    <main class="flex p-4 h-screen w-screen bg-(--color-container)">
         <section class="w-3/6">
             <img class="h-full rounded-3xl" src="{{ asset('images/assets/sun-tornado.png') }}" alt="Fundo">
         </section>
@@ -9,23 +9,33 @@
             <img class="w-56" src="{{ asset('images/assets/logo.png') }}" alt="logo">
 
             <div class="h-3/5 w-2/3">
-                <h2 class="">Acessar Conta</h2>
+                <div>
+                    <h2 class="font-(family-name:--space) font-bold text-xl">Acessar Conta</h2>
+                    <div class="w-8 h-0.5 mt-0.5 bg-orange-500"></div>
+                </div>
 
-                <form action="{{ route('login.submit') }}" method="post">
+                <form action="{{ route('login.submit') }}" method="post" class="flex flex-col justify-around gap-10 mt-8 font-(family-name:--outfit)">
                     @csrf
 
-                    <div class="flex flex-col">
+                    <div class="flex flex-col gap-3">
                         <label for="email">E-mail</label>
-                        <input type="email" name="email">
+                        <label class="floating-label">
+                            <span>Seu E-mail</span>
+                            <input type="text" name="email" placeholder="Digite o seu E-mail" class="bg-(--scure) h-10 rounded-xl input input-md focus:outline-0">
+                        </label>
 
                         @error('email')
                             error email
                         @enderror
                     </div>
 
-                    <div class="flex flex-col">
-                        <label for="password">Password</label>
-                        <input type="text" name="password">
+                    <div class="flex flex-col gap-3">
+                        <label for="password">Senha</label>
+                        <label for="password" class="floating-label">
+                            <span>Sua Senha</span>
+                            <input type="text" name="password" placeholder="Digite sua senha" class="bg-(--scure) h-10 rounded-xl input input-md focus:outline-0">
+                        </label>
+
                         @error('password')
                             error password
                         @enderror
